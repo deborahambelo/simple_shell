f1cd22a4869812d34cbc178af90ccc40134afd49
deborahambelo <101412212+deborahambelo@users.noreply.github.com>
2023-11-13 15:32:19 +0530

    Update empty_cmd.c

--- empty_cmd.c
+++ empty_cmd.c
@@ -1,7 +1,6 @@
 #include "shell.h"
-
 /**
- * is_empty_command - Check if no command was entered
+ *is_empty_command - Check if no command was entered
  * @commands: Array of commands
  *
  * Return: 1 if no command was entered (just spaces and ENTER), 0 otherwise
@@ -10,6 +9,5 @@ int is_empty_command(char **commands)
 {
 	if (commands[0] == NULL || compare_strings(commands[0], "\n") == 0)
 		return (1);
-
 	return (0);
 }
